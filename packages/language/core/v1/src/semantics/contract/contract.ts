f5c8d60bd2916f80f4502cae810f339b090a2622
nhenin <nicolas.henin@iohk.io>
2023-08-09 15:06:14 +0200

    fixed issues with Contract

---
+++ packages/language/core/v1/src/semantics/contract/contract.ts
@@ -0,0 +1,27 @@
+
+import * as t from "io-ts";
+
+import { Assert } from "./assert.js";
+import { Close } from "./close.js";
+import { If } from "./if.js";
+import { Let } from "./let.js";
+import { Pay } from "./pay.js";
+import { When } from "./when/index.js";
+
+export type Contract =
+  | Close
+  | Pay
+  | If
+  | When
+  | Let
+  | Assert
+
+export const Contract : t.Type<Contract>
+  = t.recursion('Contract', () =>
+      t.union ([ Close
+              , Pay
+              , If
+              , When
+              , Let
+              ,  Assert]))
+
