2ece18dbad0077314791bb62c87d9e2b4ef8cf72
Hernan Rajchert <hrajchert@gmail.com>
2023-09-07 17:53:15 -0300

    PLT-7308 - remove fp-ts from wallet package
    The packages depending on this are broken and will be fixed in their own fp-ts removal.

--- packages/wallet/src/api.ts
+++ packages/wallet/src/api.ts
@@ -32,23 +32,40 @@ export type CIP30Network = "Mainnet" | "Testnets";
  * The WalletAPI is an interface for interacting with a Cardano wallet.
  */
 export interface WalletAPI {
-  // DISCUSSION: Should this function receive a timeout?
+  // DISCUSSION: Should this function receive a timeout and checkinterval?
+  // DISCUSSION: Should this be a part of the base API or an extended functionality on top of
+  //              `getUTxOs` similar to `getAddressesAndCollaterals`
   /**
    * Waits for a transaction to be confirmed.
    * @experimental
    * @param txHash the transaction hash
    * @returns true if the transaction is confirmed, false otherwise
    */
-  waitConfirmation(txHash: string): TE.TaskEither<Error, boolean>;
+  waitConfirmation(txHash: string): Promise<boolean>;
+  // DISCUSSION: should we create different Nominal types for different CBOR representations and mix and match accordingly?
+  //             maybe the WalletAPI should be generic on the CBOR representation, and it is necesary to make sure that the
+  //             generation and consumption of those CBOR are compatible.
+  // TODO: See if we can we improve the Error? CIP30 talks about DataSignError and TxSignError
+  //  https://github.com/cardano-foundation/CIPs/tree/master/CIP-0030#datasignerror
+  //  https://github.com/cardano-foundation/CIPs/tree/master/CIP-0030#txsignerror
   /**
-   * TODO: Explain
-   * @param tx TODO Explain
-   * @returns TODO Explain
+   * Ask the wallet to sign a transaction
+   * @param tx A CBOR encoded transaction
+   * @returns The signed transaction or throws
+   * @throws `Error` if there was a problem signing the transaction
+   *
    * @experimental
+   *
+   * @remarks
+   * CIP30 expects a CBOR Transaction, which consists of <txBody + witness>. The
+   * Marlowe runtime default flow returns just the txBody, to get what CIP30 expects
+   * you need to pass an Accept header when creating a contract or applying inputs.
+   * The header is `Accept "application/vendor.iog.marlowe-runtime.contract-tx-json"`
+   * and it is automatically included in the @marlowe.io/runtime-* packages.
    */
   signTxTheCIP30Way(
     tx: MarloweTxCBORHex
-  ): TE.TaskEither<Error, HexTransactionWitnessSet>;
+  ): Promise<HexTransactionWitnessSet>;
   /**
    * Returns an address to be used as Change Address when balancing transactions.
    */
@@ -75,6 +92,8 @@ export interface WalletAPI {
    */
   getCIP30Network(): Promise<CIP30Network>;
   // DISCUSSION: should we rename this function to getAssets?
+  // DISCUSSION: Being this a Marlowe Wallet API, should we return a Marlowe Asset (Marlowe Token + Quantity) instead of
+  //             CIP30 Token?
   /**
    * Get the tokens available to the wallet.
    * @experimental
@@ -82,13 +101,13 @@ export interface WalletAPI {
    * @remarks
    * The `Token` type here is different from the `Token` type in the language package.
    */
-  getTokens(): Promise<E.Either<Error, Token[]>>;
+  getTokens(): Promise<Token[]>;
   // DISCUSSION: Should we make this a separate function similar to `getAddressesAndCollaterals`?
   /**
    * A helper around `getTokens` that returns only the lovelace ammount.
    * @experimental
    */
-  getLovelaces(): Promise<E.Either<Error, bigint>>;
+  getLovelaces(): Promise<bigint>;
 }
 
 /**
