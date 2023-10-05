10de76fde7185e185be1ca3a0736347ba4541eb5
Björn Kihlberg <bjorn.wilhelm.kihlberg@gmail.com>
2023-10-05 15:29:33 +0200

    fetch all contract pages

--- packages/runtime/lifecycle/src/generic/contracts.ts
+++ packages/runtime/lifecycle/src/generic/contracts.ts
@@ -30,7 +30,8 @@ import { FPTSRestAPI } from "@marlowe.io/runtime-rest-client";
 import { DecodingError } from "@marlowe.io/adapter/codec";
 import { TransactionTextEnvelope } from "@marlowe.io/runtime-rest-client/contract/transaction/endpoints/collection";
 import { Next, noNext } from "@marlowe.io/language-core-v1/next";
-import { isNone, none } from "fp-ts/lib/Option.js";
+import { isNone, none, Option } from "fp-ts/lib/Option.js";
+import { ContractsRange } from "@marlowe.io/runtime-rest-client/contract/index";
 
 export function mkContractLifecycle(
   wallet: WalletAPI,
@@ -87,15 +88,27 @@ const getApplicableInputs =
 const getContractIds =
   ({ rest, wallet }: ContractsDI) =>
   async (): Promise<ContractId[]> => {
-    const changeAddress = await wallet.getChangeAddress();
-    const usedAddress = await wallet.getUsedAddresses();
-    const result = await rest.contracts.getHeadersByRange(none)({
-      tags: [],
-      partyAddresses: [changeAddress, ...usedAddress],
-    })();
-    if (result._tag === "Left") throw result.left;
-    const response = result.right;
-    return response.headers.map(({ contractId }) => contractId);
+    const partyAddresses = [
+      await wallet.getChangeAddress(),
+      ...(await wallet.getUsedAddresses()),
+    ];
+    const kwargs = { tags: [], partyAddresses };
+    const loop = async (
+      range: Option<ContractsRange>,
+      acc: ContractId[]
+    ): Promise<ContractId[]> => {
+      const result = await rest.contracts.getHeadersByRange(range)(kwargs)();
+      if (result._tag === "Left") throw result.left;
+      const response = result.right;
+      const contractIds = [
+        ...acc,
+        ...response.headers.map(({ contractId }) => contractId),
+      ];
+      return response.nextRange._tag === "None"
+        ? contractIds
+        : loop(response.nextRange, contractIds);
+    };
+    return loop(none, []);
   };
 
 const getParties: (
