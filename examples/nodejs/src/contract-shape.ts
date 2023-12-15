eddf0dd5a8092c68751ecaab8262a2814d43b479
Hernan Rajchert <hrajchert@gmail.com>
2023-12-15 20:47:33 -0300

    wip

--- examples/nodejs/src/contract-shape.ts
+++ examples/nodejs/src/contract-shape.ts
@@ -9,7 +9,7 @@ const WhenSingleDeposit = G.When({
   when: t.tuple([G.NormalCase({ case: GC.Deposit })]),
 });
 
-// Two example contracts, the first one doesn't match the shape, the second one does.
+// Three example contracts, only the second match the expected shape.
 const alice = { role_token: "alice" };
 const contract1: unknown = {
   when: [],
@@ -31,16 +31,44 @@ const contract2: unknown = {
   timeout: 20n,
   timeout_continuation: "close",
 };
+const contract3: unknown = {
+  when: [
+    {
+      case: {
+        party: alice,
+        deposits: 10n,
+        of_token: lovelace,
+        into_account: alice,
+      },
+      then: "close",
+    },
+    {
+      case: {
+        party: alice,
+        deposits: 20n,
+        of_token: lovelace,
+        into_account: alice,
+      },
+      then: "close",
+    },
+  ],
+  timeout: 20n,
+  timeout_continuation: "close",
+};
 
 // This logs:
-// -
-[contract1, contract2].forEach((contract, ix) => {
+//   Contract 0 is not a When with a single deposit
+//   Contract 1 is a When with a single deposit
+//   Deposit 10n
+//   Contract 2 is not a When with a single deposit
+[contract1, contract2, contract3].forEach((contract, ix) => {
   if (WhenSingleDeposit.is(contract)) {
     contract.when[0].then; // unknown
+    contract.when[0].case; // typed as Deposit
+    // this fails at compile time, if
+    // contract.when[1].case
     console.log(`Contract ${ix} is a When with a single deposit`);
     console.log(`Deposit`, contract.when[0].case.deposits);
-    // this fails at compile time
-    // contract.when[1].case
   } else {
     console.log(`Contract ${ix} is not a When with a single deposit`);
   }
