bd8a1d380fbd9dc524ec4e659a6755651645ee10
Hernan Rajchert <hrajchert@gmail.com>
2023-12-19 10:30:00 -0300

    Fix doc warnings

--- packages/runtime/client/rest/src/payout/index.ts
+++ packages/runtime/client/rest/src/payout/index.ts
@@ -1,3 +1,5 @@
 export * from "./details.js";
 export * from "./header.js";
 export * from "./status.js";
+export * from "./endpoints/collection.js";
+export * from "./endpoints/singleton.js";
--- packages/runtime/client/rest/src/withdrawal/index.ts
+++ packages/runtime/client/rest/src/withdrawal/index.ts
@@ -1,2 +1,4 @@
 export * from "./header.js";
 export * from "./details.js";
+export * from "./endpoints/collection.js";
+export * from "./endpoints/singleton.js";
